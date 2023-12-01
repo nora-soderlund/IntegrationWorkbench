227266937e025d48b7b097106fb854f839f73e1e
Nora Söderlund <nora.soderlund@polestar.com>
2023-12-01 23:00:01 +0100

    Update response

--- src/webview/responses/http/HttpFailedResponse.tsx
+++ src/webview/responses/http/HttpFailedResponse.tsx
@@ -7,36 +7,30 @@ import { HttpResponseProps } from "./HttpResponse";
 
 export default function HttpFailedResponse({ responseData }: HttpResponseProps) {
   return (
-    <React.Fragment>
-      <VSCodePanels style={{
-        flex: 1,
-        padding: "0 20px"
-      }}>
-        <VSCodePanelTab>ERRORS</VSCodePanelTab>
+    <div style={{
+      flex: 1,
+      padding: "10px 20px"
+    }}>
+      <div className="infobox infobox-error">
+        <div>
+          <i className="codicon codicon-error"></i>{" "}<b>An error occured:</b>
+          
+          <p>
+            {responseData.error}
+          </p>
+        </div>
 
-        <VSCodePanelView>
-          <div className="infobox infobox-error">
-            <div>
-              <i className="codicon codicon-error"></i>{" "}<b>An error occured:</b>
-              
-              <p>
-                {responseData.error}
-              </p>
-            </div>
-
-            <VSCodeButton onClick={(() => 
-              window.vscode.postMessage({
-                command: "integrationWorkbench.showOutputLogs",
-                arguments: []
-              })
-            )} style={{
-              width: "max-content"
-            }}>
-              Show output logs
-            </VSCodeButton>
-          </div>
-        </VSCodePanelView>
-      </VSCodePanels>
-    </React.Fragment>
+        <VSCodeButton onClick={(() => 
+          window.vscode.postMessage({
+            command: "integrationWorkbench.showOutputLogs",
+            arguments: []
+          })
+        )} style={{
+          width: "max-content"
+        }}>
+          Show output logs
+        </VSCodeButton>
+      </div>
+    </div>
   );
 }
